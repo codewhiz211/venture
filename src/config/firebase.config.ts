b5aeb26d487599f7e4c5f38f350fd49ea746162e
codewhiz211 <codewhiz211>
2024-05-30 07:58:13 -0700

    remove firebase api key

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -2,12 +2,12 @@ const databaseURL = 'https://vd-deploy-test.firebaseio.com';
 const projectId = 'vd-deploy-test';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCBFTpdGLZIG0ntU0yhI19LRyb1HFLDYZY',
+  apiKey: '',
   authDomain: 'vd-deploy-test.firebaseapp.com',
   projectId,
   databaseURL,
   storageBucket: 'vd-deploy-test.appspot.com',
-  messagingSenderId: '1000298240393',
+  messagingSenderId: '',
 };
 
 export { firebaseConfig, databaseURL, projectId };
